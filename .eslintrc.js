8863e7fef42313e88198223aa9a7999174b8b73d
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-10 12:01:30 -0300

    feat: add eslint
    add eslint with airbnb-base style

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': 'off',
+  },
+};
