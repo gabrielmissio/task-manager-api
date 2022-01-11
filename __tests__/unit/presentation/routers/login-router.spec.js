764f8fa0b550aae2cefa5e399995e0222796a6a0
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-11 11:40:01 -0300

    feat: ensure LoginRouter returns statusCode 500 and body with Internal Server Error message when the provided httpRequest has no body

--- __tests__/unit/presentation/routers/login-router.spec.js
+++ __tests__/unit/presentation/routers/login-router.spec.js
@@ -28,4 +28,19 @@ describe('Given the LoginRouter', () => {
       expect(response.body).toBe('Internal Server Error');
     });
   });
+
+  describe('And httpRequest has no body', () => {
+    let response;
+    beforeAll(async () => {
+      const { sut } = makeSut();
+      response = await sut.handler({});
+    });
+
+    test('Then I expect it returns statusCode 500', () => {
+      expect(response.statusCode).toBe(500);
+    });
+    test('Then I expect it returns the body with Internal Server Error message', () => {
+      expect(response.body).toBe('Internal Server Error');
+    });
+  });
 });
