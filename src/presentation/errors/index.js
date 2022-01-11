2e9de5d535e0691f6fae44dcce6c5bbb576755cc
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-11 17:19:52 -0300

    feat: add UnauthorizedError to presentation errors

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,7 +1,9 @@
 const InvalidRequestError = require('./invalid-request-error');
 const InternalServerError = require('./internal-server-error');
+const UnauthorizedError = require('./unauthorized-error');
 
 module.exports = {
   InvalidRequestError,
-  InternalServerError
+  InternalServerError,
+  UnauthorizedError
 };
