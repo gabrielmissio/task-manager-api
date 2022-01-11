ecc958d10077150d1b45b3b57cd91e77b1ca87d8
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-11 11:36:28 -0300

    feat: ensure LoginRouter returns statusCode 500 and body with Internal Server Error message when no httpRequest was provided

---
+++ __tests__/unit/presentation/routers/login-router.spec.js
@@ -0,0 +1,31 @@
+const { HttpResponse } = require('../../../../src/presentation/helpers');
+
+class LoginRouter {
+  async handler() {
+    return HttpResponse.exceptionHandler({});
+  }
+}
+
+const makeSut = () => {
+  const sut = new LoginRouter();
+  return {
+    sut
+  };
+};
+
+describe('Given the LoginRouter', () => {
+  describe('And no httpRequest is provided', () => {
+    let response;
+    beforeAll(async () => {
+      const { sut } = makeSut();
+      response = await sut.handler();
+    });
+
+    test('Then I expect it returns statusCode 500', () => {
+      expect(response.statusCode).toBe(500);
+    });
+    test('Then I expect it returns the body with Internal Server Error message', () => {
+      expect(response.body).toBe('Internal Server Error');
+    });
+  });
+});
