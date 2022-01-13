9db0f3aeebc1c5ffd53528c9b21d74d7c313d748
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-12 22:11:59 -0300

    feat: add getInteger method to DataFakerHelper

--- __tests__/helpers/data-faker-helper.js
+++ __tests__/helpers/data-faker-helper.js
@@ -3,14 +3,16 @@ const Chance = require('chance');
 const dataFaker = new Chance();
 
 class DataFakerHelper {
-  static getString(config) {
-    const { length, pool, alpha, casing, symbols } = config;
+  static getString({ length, pool, alpha, casing, symbols }) {
     return dataFaker.string({ length, pool, alpha, casing, symbols });
   }
 
-  static getSentence(config) {
-    const { words } = config;
+  static getSentence({ words }) {
     return dataFaker.sentence({ words });
+  }
+
+  static getInteger({ min, max }) {
+    return dataFaker.integer({ min, max });
   }
 }
 
