ead0971f5aa9e2771b835c144f337f312e4b7935
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-13 21:06:48 -0300

    feat: add getObject method to DataFakerHelper

--- __tests__/helpers/data-faker-helper.js
+++ __tests__/helpers/data-faker-helper.js
@@ -3,16 +3,29 @@ const Chance = require('chance');
 const dataFaker = new Chance();
 
 class DataFakerHelper {
-  static getString({ length, pool, alpha, casing, symbols }) {
+  static getString({ length, pool, alpha, casing, symbols } = {}) {
     return dataFaker.string({ length, pool, alpha, casing, symbols });
   }
 
-  static getSentence({ words }) {
+  static getSentence({ words } = {}) {
     return dataFaker.sentence({ words });
   }
 
-  static getInteger({ min, max }) {
+  static getInteger({ min, max } = {}) {
     return dataFaker.integer({ min, max });
+  }
+
+  static getObject({ length } = { length: 1 }) {
+    const newObject = {};
+    const pool = 'abcdefghijklmnopqrstuvwxyz';
+    const getKeyOrValueSettings = { length: 4, pool };
+    for (let index = 0; index < length; index += 1) {
+      Object.assign(newObject, {
+        [this.getString(getKeyOrValueSettings)]: this.getString(getKeyOrValueSettings)
+      });
+    }
+
+    return newObject;
   }
 }
 
