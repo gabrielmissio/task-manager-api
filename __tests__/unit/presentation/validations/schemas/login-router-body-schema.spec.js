c7b4eebfbd040789dda7813953867f68c515b13b
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-14 11:55:18 -0300

    feat(presentation): ensure LoginRouterBodySchema returns "email" is required in the error message when no email is provided

--- __tests__/unit/presentation/validations/schemas/login-router-body-schema.spec.js
+++ __tests__/unit/presentation/validations/schemas/login-router-body-schema.spec.js
@@ -1,6 +1,12 @@
 const Joi = require('joi');
 
-const LoginRouterBodySchema = Joi.object({});
+const {
+  Commons: { emailValidation }
+} = require('../../../../../src/presentation/validations/helpers');
+
+const LoginRouterBodySchema = Joi.object({
+  email: emailValidation.required()
+});
 
 const makeSut = () => {
   const sut = LoginRouterBodySchema;
@@ -12,11 +18,20 @@ const makeSut = () => {
 
 describe('Given the LoginRouterBodySchema', () => {
   describe('And no parameters are provided', () => {
-    test("Then I expect it returns an object with a 'value' key with undefined value", () => {
+    test('Then I expect it returns an object with a "value" key with undefined value', () => {
       const { sut } = makeSut();
       const response = sut.validate();
 
       expect(response.value).toBeUndefined();
+    });
+  });
+
+  describe('And no email is provided', () => {
+    test('Then I expect it returns "email" is required in the error message', () => {
+      const { sut } = makeSut();
+      const response = sut.validate({});
+
+      expect(response.error.message).toBe('"email" is required');
     });
   });
 });
