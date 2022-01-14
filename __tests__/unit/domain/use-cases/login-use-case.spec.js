47a5bcc875fd539cb8c5e9dcfcb16d37ad836c57
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-14 17:39:15 -0300

    feat(domain): ensure LoginUseCase throws a MissingParamError when no password is provided

--- __tests__/unit/domain/use-cases/login-use-case.spec.js
+++ __tests__/unit/domain/use-cases/login-use-case.spec.js
@@ -1,8 +1,9 @@
 const { MissingParamError } = require('../../../../src/utils/errors');
 
 class LoginUseCase {
-  async handler() {
-    throw new MissingParamError('email');
+  async handler({ email }) {
+    if (!email) throw new MissingParamError('email');
+    throw new MissingParamError('password');
   }
 }
 
@@ -18,9 +19,19 @@ describe('Given the LoginUseCase', () => {
   describe('And no email is provided', () => {
     test('Then I expect it throws a MissingParamError', async () => {
       const { sut } = makeSut();
-      const promise = sut.handler();
+      const promise = sut.handler({});
 
-      expect(promise).rejects.toThrow(new MissingParamError('email'));
+      await expect(promise).rejects.toThrow(new MissingParamError('email'));
+    });
+  });
+
+  describe('And no password is provided', () => {
+    test('Then I expect it throws a MissingParamError', async () => {
+      const { sut } = makeSut();
+      const params = { email: 'any' };
+      const promise = sut.handler(params);
+
+      await expect(promise).rejects.toThrow(new MissingParamError('password'));
     });
   });
 });
