3faac6c0d0a44e73e36dde33475098d408384b12
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-19 14:42:38 -0300

    feat(infra): add get method to DynamodbClient class

---
+++ src/infra/db/dynamodb/helpers/dynamodb-client.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+
+const REGION = process.env.REGION || 'localhost';
+const DYNAMODB_ENDPOINT = process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000';
+
+const CLIENT = new AWS.DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10',
+  endpoint: DYNAMODB_ENDPOINT,
+  region: REGION
+});
+
+class DynamodbClient {
+  static async get(params) {
+    const response = await CLIENT.get(params).promise();
+
+    const error = this.getResponseError(response);
+    if (error) throw new Error(error);
+
+    return response;
+  }
+
+  static getResponseError({ $response }) {
+    return $response && $response.error;
+  }
+}
+
+module.exports = DynamodbClient;
