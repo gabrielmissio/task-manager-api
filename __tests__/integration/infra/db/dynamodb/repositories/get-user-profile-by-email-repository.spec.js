c8af633f8a00b9b695929d7dc38072d8cf251676
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-21 16:55:06 -0300

    feat(infra-dynamodb): ensure GetUserProfileByEmailRepository returns null when there is no user with the provided email in the database

---
+++ __tests__/integration/infra/db/dynamodb/repositories/get-user-profile-by-email-repository.spec.js
@@ -0,0 +1,62 @@
+const { MissingParamError } = require('../../../../../../src/utils/errors');
+const { DynamodbClient } = require('../../../../../../src/infra/db/dynamodb/helpers');
+const { TASK_MANAGER_TABLE_NAME } = require('../../../../../../src/main/confing/env');
+const { DataFakerHelper } = require('../../../../../helpers');
+
+class GetUserProfileByEmailRepository {
+  async get({ email }) {
+    const params = this.buildParams({ email });
+    const dynamodbResponse = await DynamodbClient.query(params);
+
+    const userNotFound = dynamodbResponse.Count < 1;
+    if (userNotFound) return null;
+
+    const userProfile = this.buildUserProfile(dynamodbResponse.Items[0]);
+    return userProfile;
+  }
+
+  buildParams({ email }) {
+    if (!email) throw new MissingParamError('email');
+    return {
+      TableName: TASK_MANAGER_TABLE_NAME,
+      IndexName: 'email-index',
+      KeyConditionExpression: 'email = :email',
+      ExpressionAttributeValues: { ':email': email }
+    };
+  }
+
+  buildUserProfile(payload) {
+    return {
+      id: payload.PK.replace('USER#', ''),
+      name: payload.name,
+      email: payload.email,
+      password: payload.password
+    };
+  }
+}
+
+const makeSut = () => {
+  const sut = new GetUserProfileByEmailRepository();
+
+  return { sut };
+};
+
+describe('Given the GetUserProfileByEmailRepository', () => {
+  describe('And no email is provided', () => {
+    test('Then I expect it throws a MissingParamError', async () => {
+      const { sut } = makeSut();
+      const promise = sut.get({});
+
+      await expect(promise).rejects.toThrow(new MissingParamError('email'));
+    });
+  });
+
+  describe('And there is no user with the provided email in the database', () => {
+    test('Then I expect it returns null', async () => {
+      const { sut } = makeSut();
+      const response = await sut.get({ email: DataFakerHelper.getEmail() });
+
+      expect(response).toBeNull();
+    });
+  });
+});
