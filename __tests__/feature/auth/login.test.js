4d013f2e3c9a1c00f147fa56b436d062b59eefc7
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-22 19:49:16 -0300

    feat: ensure that /login POST returns 200 when valid credentials are provided

--- __tests__/feature/auth/login.test.js
+++ __tests__/feature/auth/login.test.js
@@ -2,11 +2,13 @@ const ROUTE = '/login';
 const request = require('supertest');
 
 const app = require('../../../src/main/confing/app');
-const { DataFakerHelper } = require('../../helpers');
+const { DataFakerHelper, ProfileDataFaker } = require('../../helpers');
+const { DynamodbClient } = require('../../../src/infra/db/dynamodb/helpers');
+const { TASK_MANAGER_TABLE_NAME } = require('../../../src/main/confing/env');
 
-const makeRequestBody = () => ({
-  email: DataFakerHelper.getEmail(),
-  password: DataFakerHelper.getPassword()
+const makeRequestBody = (profileFake = {}) => ({
+  email: profileFake.email || DataFakerHelper.getEmail(),
+  password: profileFake.password || DataFakerHelper.getPassword()
 });
 
 describe(`Given the ${ROUTE} route`, () => {
@@ -53,6 +55,23 @@ describe(`Given the ${ROUTE} route`, () => {
 
       test('Then I expect it retuns statud code 401', async () => {
         expect(response.status).toBe(401);
+      });
+    });
+
+    describe('And valid credentials are provided', () => {
+      let response;
+      const profileFake = ProfileDataFaker.getProfile();
+      const requestBody = makeRequestBody(profileFake);
+      beforeAll(async () => {
+        await DynamodbClient.put({
+          TableName: TASK_MANAGER_TABLE_NAME,
+          Item: profileFake
+        });
+        response = await request(app).post(ROUTE).send(requestBody);
+      });
+
+      test('Then I expect it retuns statud code 200', async () => {
+        expect(response.status).toBe(200);
       });
     });
   });
