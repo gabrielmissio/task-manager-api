5597ee1f44d8da435c7111744f8c696368bede9c
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-22 17:52:06 -0300

    feat(main): add PORT to env

--- src/main/confing/env.js
+++ src/main/confing/env.js
@@ -1,4 +1,5 @@
 module.exports = {
+  PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'dev',
   REGION: process.env.REGION || 'localhost',
   SECRET: process.env.SECRET || 'secret',
