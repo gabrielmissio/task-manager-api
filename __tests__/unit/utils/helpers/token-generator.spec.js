0ad3860c9766fe0cccc6dc1a3f18239833d7e440
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-22 14:47:57 -0300

    feat: ensure TokenGenerator throws a new MissingParamError when no value is porvided

--- __tests__/unit/utils/helpers/token-generator.spec.js
+++ __tests__/unit/utils/helpers/token-generator.spec.js
@@ -1,13 +1,18 @@
 const { MissingParamError } = require('../../../../src/utils/errors');
 
 class TokenGenerator {
+  constructor({ secret } = {}) {
+    this.secret = secret;
+  }
+
   async generate() {
-    throw new MissingParamError('secret');
+    if (!this.secret) throw new MissingParamError('secret');
+    throw new MissingParamError('value');
   }
 }
 
 const makeSut = () => {
-  const sut = new TokenGenerator();
+  const sut = new TokenGenerator({ secret: 'any_secret' });
 
   return { sut };
 };
@@ -15,10 +20,19 @@ const makeSut = () => {
 describe('Given the TokenGenerator', () => {
   describe('And the secret parameter was not injected', () => {
     test('Then I expect it throws a new MissinParamError', async () => {
-      const { sut } = makeSut();
+      const sut = new TokenGenerator();
       const promise = sut.generate();
 
       await expect(promise).rejects.toThrow(new MissingParamError('secret'));
+    });
+  });
+
+  describe('And no value is provided', () => {
+    test('Then I expect it throws a new MissinParamError', async () => {
+      const { sut } = makeSut();
+      const promise = sut.generate();
+
+      await expect(promise).rejects.toThrow(new MissingParamError('value'));
     });
   });
 });
