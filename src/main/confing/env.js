e3db97ac741e05ae472c08896e1f08a870a62c38
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-22 17:07:11 -0300

    feat(main): add SECRET to env

--- src/main/confing/env.js
+++ src/main/confing/env.js
@@ -1,6 +1,7 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'dev',
   REGION: process.env.REGION || 'localhost',
+  SECRET: process.env.SECRET || 'secret',
   DYNAMODB_ENDPOINT: process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000',
   TASK_MANAGER_TABLE_NAME: process.env.USER_TABLE_NAME || 'task-manager'
 };
