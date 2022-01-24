e868bcd367f60bcb652885dcfe69b52418ee0c4a
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-24 16:06:55 -0300

    feat(presentation): ensure GetBooksAndRelatedTasksController returns statusCode 500 and the body with InternalServerError message when requestParamsValidator dependency has no validate method

--- __tests__/unit/presentation/controllers/get-books-and-related-tasks-controller.spec.js
+++ __tests__/unit/presentation/controllers/get-books-and-related-tasks-controller.spec.js
@@ -3,6 +3,10 @@ const { InternalServerError } = require('../../../../src/presentation/errors');
 const { HttpResponse } = require('../../../../src/presentation/helpers');
 
 class GetBooksAndRelatedTasksController {
+  constructor({ requestParamsValidator } = {}) {
+    this.requestParamsValidator = requestParamsValidator;
+  }
+
   async handler() {
     try {
       throw new MissingParamError('params');
@@ -35,10 +39,25 @@ describe('Given the GetBooksAndRelatedTasksController', () => {
     });
   });
 
-  describe('And the requestPamasValidator dependency is not injected', () => {
+  describe('And the requestParamsValidator dependency is not injected', () => {
     let response;
     beforeAll(async () => {
-      const { sut } = makeSut();
+      const sut = new GetBooksAndRelatedTasksController();
+      response = await sut.handler({ params: 'any_uuid' });
+    });
+
+    test('Then I expect it returns statusCode 500', async () => {
+      expect(response.statusCode).toBe(500);
+    });
+    test('Then I expect it returns the body with InternalServerError message', () => {
+      expect(response.body).toEqual({ error: new InternalServerError().message });
+    });
+  });
+
+  describe('And the requestParamsValidator dependency has no validate method', () => {
+    let response;
+    beforeAll(async () => {
+      const sut = new GetBooksAndRelatedTasksController({ requestParamsValidator: {} });
       response = await sut.handler({ params: 'any_uuid' });
     });
 
