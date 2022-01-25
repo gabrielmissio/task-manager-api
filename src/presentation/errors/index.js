b5d137c2ea9578636f1cf290e6fe5c3799f8f551
gabrielmissio <gabmissil@gmail.com>
2022-01-24 21:08:15 -0300

    feat: add NotFoundError to presentation errors

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,9 +1,11 @@
 const InvalidRequestError = require('./invalid-request-error');
 const InternalServerError = require('./internal-server-error');
+const NotFoundError = require('./not-found-error');
 const UnauthorizedError = require('./unauthorized-error');
 
 module.exports = {
   InvalidRequestError,
   InternalServerError,
+  NotFoundError,
   UnauthorizedError
 };
