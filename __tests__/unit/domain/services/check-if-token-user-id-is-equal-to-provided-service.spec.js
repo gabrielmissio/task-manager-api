1590e0d61443665d726bd132832f040655e8c8ce
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-26 13:55:23 -0300

    feat(domain): ensure CheckIfTokenUserIdIsEqualToProvidedService throws an error when the tokenDecoder dependency has no decode method

--- __tests__/unit/domain/services/check-if-token-user-id-is-equal-to-provided-service.spec.js
+++ __tests__/unit/domain/services/check-if-token-user-id-is-equal-to-provided-service.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../../../../src/utils/errors');
 
 class CheckIfTokenUserIdIsEqualToProvidedService {
+  constructor({ tokenDecoder } = {}) {
+    this.tokenDecoder = tokenDecoder;
+  }
+
   handler({ userId, token }) {
     if (!userId) throw new MissingParamError('userId');
     if (!token) throw new MissingParamError('token');
@@ -37,7 +41,7 @@ describe('Given the CheckIfTokenUserIdIsEqualToProvidedService', () => {
   });
 
   describe('And the tokenDecoder dependency is not injected', () => {
-    test('Then I expect it throws an error', async () => {
+    test('Then I expect it throws an error', () => {
       const sut = new CheckIfTokenUserIdIsEqualToProvidedService();
       const params = {
         userId: 'any_email',
@@ -47,6 +51,20 @@ describe('Given the CheckIfTokenUserIdIsEqualToProvidedService', () => {
       const response = () => sut.handler(params);
 
       expect(response).toThrow(new Error("Cannot read property 'decode' of undefined"));
+    });
+  });
+
+  describe('And the tokenDecoder dependency has not decode method', () => {
+    test('Then I expect it throws an error', () => {
+      const sut = new CheckIfTokenUserIdIsEqualToProvidedService({ tokenDecoder: {} });
+      const params = {
+        userId: 'any_email',
+        token: 'any_token'
+      };
+
+      const response = () => sut.handler(params);
+
+      expect(response).toThrow(new Error('this.tokenDecoder.decode is not a function'));
     });
   });
 });
