9aba55a1d5d20ba3318624cdfb7bd23c4c1104b2
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-01-26 13:46:04 -0300

    feat(domain): ensure CheckIfTokenUserIdIsEqualToProvidedService throws a new MissingParamError when no token is provided

--- __tests__/unit/domain/services/check-if-token-user-id-is-equal-to-provided-service.spec.js
+++ __tests__/unit/domain/services/check-if-token-user-id-is-equal-to-provided-service.spec.js
@@ -1,8 +1,9 @@
 const { MissingParamError } = require('../../../../src/utils/errors');
 
 class CheckIfTokenUserIdIsEqualToProvidedService {
-  handler() {
-    throw new MissingParamError('userId');
+  handler({ userId }) {
+    if (!userId) throw new MissingParamError('userId');
+    throw new MissingParamError('token');
   }
 }
 
@@ -16,9 +17,20 @@ describe('Given the CheckIfTokenUserIdIsEqualToProvidedService', () => {
   describe('And no userId is provided', () => {
     test('Then I expect it throws a new MissingParamError', () => {
       const { sut } = makeSut();
-      const response = () => sut.handler();
+      const params = {};
+      const response = () => sut.handler(params);
 
       expect(response).toThrow(new MissingParamError('userId'));
+    });
+  });
+
+  describe('And no token is provided', () => {
+    test('Then I expect it throws a new MissingParamError', () => {
+      const { sut } = makeSut();
+      const params = { userId: 'any_id' };
+      const response = () => sut.handler(params);
+
+      expect(response).toThrow(new MissingParamError('token'));
     });
   });
 });
