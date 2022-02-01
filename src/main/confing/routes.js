b97822301d3c9aae2b2f3d43aeb41bcaea7d6535
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-02-01 20:57:11 -0300

    feat(main): add Auth middleware to book routes

--- src/main/confing/routes.js
+++ src/main/confing/routes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const { Auth } = require('../middlewares');
 const { authRoutes, bookRoutes } = require('../routes');
 
 module.exports = (app) => {
   app.use('/', router);
   authRoutes(router);
+
+  router.use(Auth);
   bookRoutes(router);
 };
