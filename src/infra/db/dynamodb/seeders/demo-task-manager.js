31c4033d447a0c5bea0c334d72602d705839ad92
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-02-01 17:43:08 -0300

    refactor(infra-dynamodb): adjust seeders to the current data format

--- src/infra/db/dynamodb/seeders/demo-task-manager.js
+++ src/infra/db/dynamodb/seeders/demo-task-manager.js
@@ -3,44 +3,44 @@ const { TASK_MANAGER_TABLE_NAME, NODE_ENV } = require('../../../../main/confing/
 
 const items = [
   {
-    PK: 'USER#001',
+    PK: 'USER#e2a138b7-409f-4092-864f-66c9b232555b',
     SK: 'PROFILE',
     name: 'John Smith',
     email: 'user@domain.com',
-    password: 'passwordhash',
+    password: '$2a$10$aT83xYBn5horL5rV76BT1enpxje/GkbwNia656qj2AzFguK28BM5q',
     createdAt: '2019-01-21T20:36:59.632Z'
   },
   {
-    PK: 'USER#001',
-    SK: 'BOOK#001',
+    PK: 'USER#e2a138b7-409f-4092-864f-66c9b232555b',
+    SK: 'BOOK#c7808a58-ddf9-42dd-b851-effc725e95a7',
     title: 'New Year Goals',
     description: 'Lore Ipsum',
     createdAt: '2019-01-21T20:36:59.632Z'
   },
   {
-    PK: 'USER#001',
-    SK: 'BOOK#002',
+    PK: 'USER#e2a138b7-409f-4092-864f-66c9b232555b',
+    SK: 'BOOK#7c870911-a0b6-4d06-897c-bbdd45faeb91',
     title: 'Carrer',
     description: 'Lore Ipsum',
     createdAt: '2019-01-21T20:36:59.632Z'
   },
   {
-    PK: 'USER#001',
-    SK: 'BOOK#001#TASK#001',
+    PK: 'USER#e2a138b7-409f-4092-864f-66c9b232555b',
+    SK: 'BOOK#c7808a58-ddf9-42dd-b851-effc725e95a7#TASK#1e66c9e3-522b-42bb-8648-906e404c73e8',
     description: 'Take a trip at least',
     isDone: false,
     createdAt: '2019-01-21T20:36:59.632Z'
   },
   {
-    PK: 'USER#001',
-    SK: 'BOOK#001#TASK#002',
+    PK: 'USER#e2a138b7-409f-4092-864f-66c9b232555b',
+    SK: 'BOOK#c7808a58-ddf9-42dd-b851-effc725e95a7#TASK#e4a20210-412b-4643-9f2a-1c68f03feace',
     description: 'Get in shape',
     isDone: false,
     createdAt: '2019-01-21T20:36:59.632Z'
   },
   {
-    PK: 'USER#001',
-    SK: 'BOOK#002#TASK#003',
+    PK: 'USER#e2a138b7-409f-4092-864f-66c9b232555b',
+    SK: 'BOOK#7c870911-a0b6-4d06-897c-bbdd45faeb91#TASK#b357e50a-3216-4041-9b9c-398bbcdc4d5d',
     description: 'Get an AWS certification',
     isDone: false,
     createdAt: '2019-01-21T20:36:59.632Z'
