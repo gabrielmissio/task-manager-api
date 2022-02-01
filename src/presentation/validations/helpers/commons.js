bc4023cb40386f597ae1c56cffdc5c3ed35d03c5
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-02-01 16:15:45 -0300

    feat(presentation): add uuidValidation to the commons module of validations helpers

--- src/presentation/validations/helpers/commons.js
+++ src/presentation/validations/helpers/commons.js
@@ -1,18 +1,24 @@
 const Joi = require('joi');
 
-const { passwordRegex } = require('../../../utils/regular-expressions');
+const { passwordRegex, uuidRegex } = require('../../../utils/regular-expressions');
 const {
-  ErrorMessagesEnum: { INVALID_PASSWORD, PASSWORD_RULES }
+  ErrorMessagesEnum: { INVALID_PASSWORD, INVALID_ID, PASSWORD_RULES }
 } = require('../../../utils/enums');
 
 const emailValidation = Joi.string().email().trim();
+
 const passwordValidation = Joi.string()
   .pattern(passwordRegex)
   .messages({
     'string.pattern.base': `${INVALID_PASSWORD}. ${PASSWORD_RULES}`
   });
 
+const uuidValidation = Joi.string().pattern(uuidRegex).messages({
+  'string.pattern.base': INVALID_ID
+});
+
 module.exports = {
   emailValidation,
-  passwordValidation
+  passwordValidation,
+  uuidValidation
 };
