1e5e87315cda933812a7e28de67b3332865bf134
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-02-01 22:01:08 -0300

    feat: ensure GetBooksAndRelatedTasks returns statudCode 401 when no authorization header is provided

---
+++ __tests__/feature/book/get-books-and-related-tasks.test.js
@@ -0,0 +1,23 @@
+const ROUTE = '/user/:userId/book';
+const request = require('supertest');
+
+const app = require('../../../src/main/confing/app');
+
+const getRoute = ({ userId } = { userId: ':userId' }) => ROUTE.replace(':userId', userId);
+
+describe(`Given the ${getRoute()} route`, () => {
+  describe('And a GET request is performed', () => {
+    describe('And no authorization header is provided', () => {
+      let response;
+      beforeAll(async () => {
+        response = await request(app)
+          .get(getRoute({ userId: 'any_userId' }))
+          .send({});
+      });
+
+      test('Then I expect it retuns status code 401', async () => {
+        expect(response.status).toBe(401);
+      });
+    });
+  });
+});
