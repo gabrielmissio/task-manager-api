3073a0410506318a58f9c401e15026cea84e3aef
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2022-02-02 11:34:47 -0300

    chore: adjust env
    adjust DYNAMODB_ENDPOINT to be null when the region is not localhost

--- src/main/confing/env.js
+++ src/main/confing/env.js
@@ -1,8 +1,10 @@
+const REGION = process.env.REGION || 'localhost';
+
 module.exports = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'dev',
-  REGION: process.env.REGION || 'localhost',
+  REGION,
   SECRET: process.env.SECRET || 'secret',
-  DYNAMODB_ENDPOINT: process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000',
-  TASK_MANAGER_TABLE_NAME: process.env.USER_TABLE_NAME || 'task-manager'
+  DYNAMODB_ENDPOINT: (REGION === 'localhost' && 'http://localhost:8000') || null,
+  TASK_MANAGER_TABLE_NAME: process.env.TASK_MANAGER_TABLE_NAME || 'task-manager'
 };
