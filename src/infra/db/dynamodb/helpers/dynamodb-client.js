32a8ac28add59b3fbe6127fad92ca526295e6fd2
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-02-04 17:57:16 -0300

    chore(infra-dynamodb): remove unused get method from DynamodbClient helper

--- src/infra/db/dynamodb/helpers/dynamodb-client.js
+++ src/infra/db/dynamodb/helpers/dynamodb-client.js
@@ -1,15 +1,6 @@
 const { DYNAMODB_DOCUMENT_CLIENT } = require('../../../../main/confing/aws-resources');
 
 class DynamodbClient {
-  static async get(params) {
-    const response = await DYNAMODB_DOCUMENT_CLIENT.get(params).promise();
-
-    const error = this.getResponseError(response);
-    if (error) throw new Error(error);
-
-    return response;
-  }
-
   static async query(params) {
     const response = await DYNAMODB_DOCUMENT_CLIENT.query(params).promise();
 
