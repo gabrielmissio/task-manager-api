b37d82d232b70a2ab2a558c33c71a6276becbd60
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-02-07 09:55:13 -0300

    feat: ensure GetBooksAndRelatedTasks returns the body with the expected fields and types when an existing userId is provided and an allowed request is provided and the provided userId has at least one book and task

--- __tests__/feature/book/get-books-and-related-tasks.test.js
+++ __tests__/feature/book/get-books-and-related-tasks.test.js
@@ -74,21 +74,49 @@ describe(`Given the ${getRoute()} route`, () => {
     });
 
     describe('And an existing userId is provided', () => {
-      let response;
-      beforeAll(async () => {
-        const token = await authenticationHelper.getAccessToken();
-        const authorization = `Bearer ${token}`;
+      describe('And an allowed request is performed', () => {
+        describe('And the provided user has at least one book and task', () => {
+          let response;
+          beforeAll(async () => {
+            const token = await authenticationHelper.getAccessToken();
+            const authorization = `Bearer ${token}`;
 
-        const userFake = await authenticationHelper.getUser();
-        response = await request(app)
-          .get(getRoute({ userId: userFake.profile.PK.replace('USER#', '') }))
-          .set({ authorization })
-          .send({});
-      });
+            const userFake = await authenticationHelper.getUser();
+            response = await request(app)
+              .get(getRoute({ userId: userFake.profile.PK.replace('USER#', '') }))
+              .set({ authorization })
+              .send({});
+          });
 
-      test('Then I expect it retuns status code 200', async () => {
-        expect(response.status).toBe(200);
+          test('Then I expect it retuns status code 200', async () => {
+            expect(response.status).toBe(200);
+          });
+
+          test('Then I expect it returns the body with expected fields and types', async () => {
+            expect(response.body).toEqual(
+              expect.arrayContaining([
+                expect.objectContaining({
+                  id: expect.any(String),
+                  title: expect.any(String),
+                  description: expect.any(String),
+                  createdAt: expect.any(String),
+                  tasks: expect.arrayContaining([
+                    expect.objectContaining({
+                      id: expect.any(String),
+                      description: expect.any(String),
+                      isDone: expect.any(Boolean),
+                      createdAt: expect.any(String)
+                    })
+                  ])
+                })
+              ])
+            );
+          });
+        });
       });
     });
+
+    // TODO: add test to ensure returns statusCode 403 when try to perform a not allowed request
+    // TODO: add test to ensure returns an empty array when the provided userId does not have any book or task
   });
 });
