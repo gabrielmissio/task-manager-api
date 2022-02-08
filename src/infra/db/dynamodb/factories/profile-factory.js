02744e09121b28a9dfae3b80373fa0e2ae3c42bc
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-02-07 22:23:35 -0300

    feat(infra-dynamodb): add buildNewProfile method to ProfileFactory

--- src/infra/db/dynamodb/factories/profile-factory.js
+++ src/infra/db/dynamodb/factories/profile-factory.js
@@ -1,3 +1,8 @@
+const { IdentifierGenerator } = require('../../../helpers');
+const { Encrypter } = require('../../../../utils/helpers');
+
+const encrypter = new Encrypter();
+
 class ProfileFactory {
   static buildProfile(payload) {
     return {
@@ -6,6 +11,21 @@ class ProfileFactory {
       email: payload.email,
       password: payload.password,
       createdAt: payload.createdAt
+    };
+  }
+
+  static async buildNewProfile(payload) {
+    const PK = `USER#${IdentifierGenerator.newUUID()}`;
+    const hash = await encrypter.hash({ value: payload.password });
+    const currentDate = new Date().toISOString();
+
+    return {
+      PK,
+      SK: 'PROFILE',
+      name: payload.name,
+      email: payload.email,
+      password: hash,
+      createdAt: currentDate
     };
   }
 }
