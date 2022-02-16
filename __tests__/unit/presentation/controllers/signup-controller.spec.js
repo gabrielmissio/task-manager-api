ff06ff0645778679e76cda6484cd2ab8086e52f8
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-02-16 17:21:42 -0300

    feat(presentation): ensure SignupController returns statusCode 500 and body with a InternalServerError message when no httpRequest is provided

---
+++ __tests__/unit/presentation/controllers/signup-controller.spec.js
@@ -0,0 +1,37 @@
+const { InternalServerError } = require('../../../../src/presentation/errors');
+const { MissingParamError } = require('../../../../src/utils/errors');
+const { HttpResponse } = require('../../../../src/presentation/helpers');
+
+class SignupController {
+  async handler() {
+    try {
+      throw new MissingParamError();
+    } catch (error) {
+      console.error(error);
+      return HttpResponse.exceptionHandler(error);
+    }
+  }
+}
+
+const makeSut = () => {
+  const sut = new SignupController();
+
+  return { sut };
+};
+
+describe('Given the SignupController', () => {
+  describe('And no httpRequest is provided', () => {
+    let response;
+    beforeAll(async () => {
+      const { sut } = makeSut();
+      response = await sut.handler();
+    });
+
+    test('Then I expect it returns statusCode 500', () => {
+      expect(response.statusCode).toBe(500);
+    });
+    test('Then I expect it returns the body with InternalServerError message', () => {
+      expect(response.body).toEqual({ error: new InternalServerError().message });
+    });
+  });
+});
